e8d3708171cc97ecc15089634accb9fa43dd4349
Mayank kumar <Mayankkumar8469@gmail.com>
2026-01-13 21:37:13 +0530

    Completed glove vs bare hand detection system

---
+++ submission/Part_1_Glove_Detection/Detection_Script.py
@@ -0,0 +1,49 @@
+import os
+import json
+import cv2
+from ultralytics import YOLO
+
+# Paths
+MODEL_PATH = "runs/detect/train2/weights/best.pt"
+INPUT_DIR = "input_images"
+OUTPUT_DIR = "output"
+LOG_DIR = "logs"
+CONFIDENCE = 0.25
+
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# Load model
+model = YOLO("runs/detect/train2/weights/best.pt")
+
+# Run inference
+results = model.predict(source=INPUT_DIR, conf=CONFIDENCE, save=True)
+
+for result in results:
+    image_name = os.path.basename(result.path)
+    detections = []
+
+    for box in result.boxes:
+        cls = int(box.cls[0])
+        label = result.names[cls]
+        conf = float(box.conf[0])
+
+        x1, y1, x2, y2 = box.xyxy[0].tolist()
+        bbox = [int(x1), int(y1), int(x2), int(y2)]
+
+        detections.append({
+            "label": label,
+            "confidence": round(conf, 3),
+            "bbox": bbox
+        })
+
+    json_data = {
+        "filename": image_name,
+        "detections": detections
+    }
+
+    json_path = os.path.join(LOG_DIR, image_name.replace(".jpg", ".json"))
+    with open(json_path, "w") as f:
+        json.dump(json_data, f, indent=4)
+
+print("Detection complete. JSON logs saved in logs/")
---
+++ submission/Part_1_Glove_Detection/gloved_hand-vs-bare_hand-2/README.dataset.txt
@@ -0,0 +1,9 @@
+# gloved_hand vs bare_hand > 2025-09-18 1:24am
+https://universe.roboflow.com/assessment-ftrxw/gloved_hand-vs-bare_hand-5d9bg-hwqst
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
+
+
+
